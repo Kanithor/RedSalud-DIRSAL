956540b73a8c56a914cfb3b9ab2057d80c62aa96
Yerson Quiroz <yerson.sarria@mail.udp.cl>
2019-06-22 22:45:45 -0400

    Start to code xls and xlsx import

--- server/models/pacienteMDL.js
+++ server/models/pacienteMDL.js
@@ -6,6 +6,7 @@ const pacienteSchema = mongoose.Schema({
     email:                       { type: String, required: false, unique: true },
     rut:                         { type: Number, required: true, trim: true, unique: true},
     active:                      { type: Boolean, required: true, default: true},
+    afiliado:                    { type: Boolean, required:  true, default: true},
     createdAt:                   { type: Date, default: Date.now }
 });
 exports.pacienteModel = mongoose.model('paciente', pacienteSchema);
